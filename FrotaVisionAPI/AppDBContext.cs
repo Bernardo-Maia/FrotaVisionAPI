18357e72f01723d8e1f495c73cb5da3c535bd7d4
72200308 <72200308@OMEGAB24>
2025-04-10 19:14:10 -0300

    Correção na conexão do banco Correção 2 do DBContext

--- FrotaVisionAPI/AppDBContext.cs
+++ FrotaVisionAPI/AppDBContext.cs
@@ -20,8 +20,7 @@ namespace FrotaVisionAPI
         {
             if (!optionsBuilder.IsConfigured)
             {
-                var connectionString = "User Id=postgres.apimpqpegxjlygictble;Password=riCaS2kVXbLOderQ;Server=aws-0-sa-east-1.pooler.supabase.com;Port=6543;Database=postgres";
-                optionsBuilder.UseNpgsql(connectionString);
+                optionsBuilder.UseNpgsql("Host=db.apimpqpegxjlygictble.supabase.co;Database=postgres;Username=postgres;Password=frotavisionPIT;SSL Mode=Require;Trust Server Certificate=true");
             }
         }
 
